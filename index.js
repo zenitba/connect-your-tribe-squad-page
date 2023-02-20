c18049f76597db90209ecb9298d4fc7887805dec
zenitba <112856019+zenitba@users.noreply.github.com>
2023-02-20 20:31:08 +0100

    basis klaar

---
+++ index.js
@@ -0,0 +1,33 @@
+import express from "express";
+
+const url = "https://whois.fdnd.nl/api/v1/squad/squat-c-2022";
+const data = await fetch(url)
+    .then((response) => response.json())
+    .catch((error) => error);
+
+console.log(data);
+
+// Maak een nieuwe express app
+const app = express();
+
+// Stel in hoe we express gebruiken
+app.set("view engine", "ejs");
+app.set("views", "./views");
+app.use(express.static("public"));
+
+// Maak een route voor de index
+app.get("/", (request, response) => {
+    console.log(request.query.squad);
+
+    response.render("index", data);
+});
+
+// app.get('/members', (request, response) => {
+//   response.send('Joepie!!')
+// })
+
+// Stel het poortnummer in en start express
+app.set("port", process.env.PORT || 1000);
+app.listen(app.get("port"), function () {
+    console.log(`Application started on http://localhost:${app.get("port")}`);
+});
